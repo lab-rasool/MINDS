f37f66abc45f9a736488741573c20b61f20eafdd
Aakash-Tripathi <aakash.tripathi0304@gmail.com>
2025-04-01 14:29:46 -0400

    Bump version to 0.1.0 and update dependencies in PKG-INFO and pyproject.toml

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ requirements = [
 
 setup(
     name="med-minds",  # Changed from minds to med-minds
-    version="0.0.8",  # PostgreSQL migration version
+    version="0.1.0",  # PostgreSQL migration version
     description="A package for downloading and processing multimodal oncology data from the MINDS database",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
